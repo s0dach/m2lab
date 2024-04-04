083d07f4ca9e3926c5cea22c6dec793a3bad3f38
John Doe <johndoe@example.com>
2024-04-04 12:19:59 +0300

    first

---
+++ components/UI/SlideOver.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <UButton label="Open" @click="isOpen = true" />
+
+    <USlideover v-model="isOpen">
+      <div class="p-4 flex-1">
+        <slot></slot>
+      </div>
+    </USlideover>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+export default {
+  name: 'slide-over'
+}
+</script>
